a989c7618a2fdc35efebc5b60a5985973fad36fd
Malte Bliefernicht <76787348+MalteBliefernicht@users.noreply.github.com>
2023-11-13 07:59:42 +0100

    change from php to html
    Discovered GitHub Pages does not support PHP.

--- templates/demo_selectionbar.php
+++
@@ -1,5 +0,0 @@
-        <div class="demo-selection">
-            <a href="/demo_cursor.php">Follow The Cursor</a>
-            <a href="">Demo B</a>
-            <a href="">Demo C</a>
-        </div>
